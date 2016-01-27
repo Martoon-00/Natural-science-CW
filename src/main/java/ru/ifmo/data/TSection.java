d0190e19c5865b2c06add98b10c78c1fddcfcd9d
Martoon-00 <Martoon_00@mail.ru>
2016-01-27 22:54:05 +0300

    removed extra

--- src/main/java/ru/ifmo/data/TSection.java
+++ src/main/java/ru/ifmo/data/TSection.java
@@ -33,13 +33,6 @@ public class TSection {
     public List<TSection> extend(List<SimpleTSection> sections) {
         //TODO: calculate TSection's basing on SimpleTSection's
 
-        //TODO: speeds (VX and VF) may be calculated as differences X(t) - X(t - 1) and XF(t) - XF(t - 1) accordingly
-
-        //TODO: XF is such x at which maximum of VS is reached. Operating with discrete representation of VS,
-        //TODO: we need to interpolate this function in order to find a peek
-        //TODO: wtf :(  my grace to guy who will implement this
-        //TODO: at least fill XSections
-
         return new ArrayList<>();
     }
 }
